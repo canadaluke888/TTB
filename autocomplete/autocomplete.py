53866e2c6322a19f39a159ba4faac86f192a4c2c
Luke <canadaluke888@gmail.com>
2024-12-07 17:56:21 -0500

    Type-hinting and docstrings for the autocomplete module.

--- autocomplete/autocomplete.py
+++ autocomplete/autocomplete.py
@@ -26,31 +26,63 @@ class Autocomplete:
         self.database_commands = self.get_database_commands()
         
     @cache
-    def load_command_list(self):
+    def load_command_list(self) -> dict:
+        """
+        Load the complete list of commands from file.
+
+        Returns:
+            dict: The dictionary containing the lists of commands for each section of the app.
+        """
         with open(self.command_list_file, 'r') as f:
             return json.load(f)
         
     @cache
-    def get_main_menu_commands(self):
+    def get_main_menu_commands(self) -> list:
+        """
+        Extracts the main menu commands from the complete list of commands.
+
+        Returns:
+            list: The complete list of main menu commands.
+        """
         return self.all_commands.get("main_menu")
     
     @cache
-    def get_settings_commands(self):
+    def get_settings_commands(self) -> list:
+        """
+        Extracts the settings commands from teh complete list of commands.
+
+        Returns:
+            list: The complete list of settings commands.
+        """
         return self.all_commands.get("settings")
     
     @cache
-    def get_table_builder_commands(self):
+    def get_table_builder_commands(self) -> list:
+        """
+        Extracts the table builder commands from the complete list of commands.
+
+        Returns:
+            list: The complete list of table builder commands.
+        """
         return self.all_commands.get("table_builder")
     
     @cache
-    def get_database_commands(self):
+    def get_database_commands(self) -> list:
+        """
+        Extracts the database commands from the complete list of commands.
+
+        Returns:
+            list: The complete list of database commands.
+        """
         return self.all_commands.get("database")
     
-    def suggest_command(self, user_input, commands):
+    def suggest_command(self, user_input: str, commands: list) -> MessagePanel:
         """
         Suggest the closest commands to the user's input.
 
         :param user_input: The command input by the user.
+        :param commands: The list of commands for the specified app section.
+        :return MessagePanel: The formatted message panel with the suggested command.
         """
         matches = get_close_matches(user_input, commands, n=1, cutoff=self.cutoff)
         if matches:
